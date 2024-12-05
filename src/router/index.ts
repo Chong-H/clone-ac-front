30ea71bd0ebdcebb20b62c6bb29f14a631650e8c
Loss0fTime <1399651432@qq.com>
2024-12-05 15:50:34 +0800

    change api name

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import UserView from '../views/UserView.vue'
+import UserView from '@/views/UserView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'user-view',
       component: UserView,
     },
-
   ],
 })
 
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <!-- 用户表单 -->
-        <form @submit.prevent="submitUser">
+        <form @submit.prevent="handleSubmitUser">
             <input v-model="user.username" placeholder="用户名" required />
             <input v-model="user.email" type="email" placeholder="邮箱" required />
             <input v-model="user.passwordHash" type="password" placeholder="密码" required />
@@ -10,74 +10,67 @@
         <!-- 用户列表 -->
         <ul>
             <li v-for="user in users" :key="user.userId">
-               {{ user.userId }} - {{ user.username }} - {{ user.email }}
-               <button @click="editUser(user)">编辑</button>
-               <button @click="deleteUser(user.userId)">删除</button>
+                {{ user.userId }} - {{ user.username }} - {{ user.email }}
+                <button @click="handleEditUser(user)">编辑</button>
+                <button @click="handleDeleteUser(user.userId)">删除</button>
             </li>
         </ul>
         <!-- 编辑表单 -->
         <div v-if="isEditing">
             <h2>编辑用户</h2>
-            <form @submit.prevent="updateUserView">
+            <form @submit.prevent="handleUpdateUser">
                 <input v-model="currentUser.username" placeholder="用户名" required />
                 <input v-model="currentUser.email" type="email" placeholder="邮箱" required />
                 <input v-model="currentUser.passwordHash" type="password" placeholder="密码" required />
                 <button type="submit">更新</button>
-                <button type="button" @click="cancelEdit">取消</button>
+                <button type="button" @click="handleCancelEdit">取消</button>
             </form>
         </div>
     </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { User } from '@/pojo/User';
-import { defineComponent, ref, onMounted, type Ref } from 'vue';
-import { getAllUsers, createUser, updateUser, deleteUser } from '@/api';
+import { ref, onMounted, type Ref } from 'vue';
+import { createUserAPI, deleteUserAPI, getAllUsersAPI, updateUserAPI } from '@/api';
 
-export default defineComponent({
-    name: 'UserView',
-    setup() {
-        const users: Ref<User[]> = ref([] as User[]);
-        const user: Ref<User> = ref({ username: '', email: '', passwordHash: '' } as User);
-        const isEditing = ref(false);
-        const currentUser: Ref<User> = ref({} as User);
+const users: Ref<User[]> = ref([] as User[]);
+const user: Ref<User> = ref({ username: '', email: '', passwordHash: '' } as User);
+const isEditing = ref(false);
+const currentUser: Ref<User> = ref({} as User);
 
-        const fetchUsers = async (): Promise<void> => {
-            users.value = (await getAllUsers()).data;
-            console.log("A user is fetched: ", users.value[0]);
-        };
+const fetchUsers = async (): Promise<void> => {
+    users.value = (await getAllUsersAPI()).data;
+    console.log("A user is fetched: ", users.value[0]);
+};
 
-        const submitUser = async (): Promise<void> => {
-            await createUser(user.value);
-            fetchUsers();
-        };
+const handleSubmitUser = async (): Promise<void> => {
+    await createUserAPI(user.value);
+    fetchUsers();
+};
 
-        const editUser = (user: User) => {
-            isEditing.value = true;
-            currentUser.value = { ...user };
-        };
+const handleEditUser = (user: User) => {
+    isEditing.value = true;
+    currentUser.value = { ...user };
+};
 
-        const updateUserView = async (): Promise<void> => {
-            await updateUser(currentUser.value.userId, currentUser.value);
-            isEditing.value = false;
-            fetchUsers();
-        };
+const handleUpdateUser = async (): Promise<void> => {
+    await updateUserAPI(currentUser.value.userId, currentUser.value);
+    isEditing.value = false;
+    fetchUsers();
+};
 
-        const deleteUserView = async (userId: number): Promise<void> => {
-            await deleteUser(userId);
-            fetchUsers();
-        };
+const handleDeleteUser = async (userId: number): Promise<void> => {
+    await deleteUserAPI(userId);
+    fetchUsers();
+};
 
-        const cancelEdit = () => {
-            isEditing.value = false;
-            currentUser.value = {} as User;
-        };
+const handleCancelEdit = () => {
+    isEditing.value = false;
+    currentUser.value = {} as User;
+};
 
-        onMounted(fetchUsers);
-
-        return { users, user, submitUser, fetchUsers, isEditing, currentUser, editUser, updateUserView, deleteUser: deleteUserView, cancelEdit };
-    }
-});
+onMounted(fetchUsers);
 </script>
 
 <style scoped></style>
