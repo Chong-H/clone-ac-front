4d700782496dc07e01e812387d59bce752129bf0
Loss0fTime <1399651432@qq.com>
2024-12-05 17:25:27 +0800

    样式修改

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,42 +1,44 @@
 <template>
     <div>
-        <!-- 用户表单 -->
-        <form @submit.prevent="handleSubmitUser">
-            <input v-model="user.username" placeholder="用户名" required />
-            <input v-model="user.email" type="email" placeholder="邮箱" required />
-            <input v-model="user.passwordHash" type="password" placeholder="密码" required />
-            <button type="submit">提交</button>
-        </form>
-
-        <!-- 查询用户拥有的藏品 -->
-        <form @submit.prevent="handleQueryUserCollectibles">
-            <input v-model="userId" type="number" placeholder="用户ID" required />
-            <button type="submit">查询</button>
-        </form>
-        <!-- 藏品列表 -->
-        <ul>
-            <li v-for="item in userCollectibles" :key="item.collectibleId">
-                {{ item.collectibleId }} - {{ item.name }} - {{ item.price }}
-            </li>
-        </ul>
-        <!-- 用户列表 -->
-        <ul>
-            <li v-for="user in users" :key="user.userId">
-                {{ user.userId }} - {{ user.username }} - {{ user.email }}
-                <button @click="handleEditUser(user)">编辑</button>
-                <button @click="handleDeleteUser(user.userId)">删除</button>
-            </li>
-        </ul>
-        <!-- 编辑表单 -->
-        <div v-if="isEditing">
-            <h2>编辑用户</h2>
-            <form @submit.prevent="handleUpdateUser">
-                <input v-model="currentUser.username" placeholder="用户名" required />
-                <input v-model="currentUser.email" type="email" placeholder="邮箱" required />
-                <input v-model="currentUser.passwordHash" type="password" placeholder="密码" required />
-                <button type="submit">更新</button>
-                <button type="button" @click="handleCancelEdit">取消</button>
+        <div class="user-layout">
+            <!-- 用户表单 -->
+            <form @submit.prevent="handleSubmitUser" class="user-form">
+                <input v-model="user.username" placeholder="用户名" required />
+                <input v-model="user.email" type="email" placeholder="邮箱" required />
+                <input v-model="user.passwordHash" type="password" placeholder="密码" required />
+                <button type="submit">提交</button>
             </form>
+
+            <!-- 查询用户拥有的藏品 -->
+            <form @submit.prevent="handleQueryUserCollectibles" class="query-form">
+                <input v-model="userId" type="number" placeholder="用户ID" required />
+                <button type="submit">查询</button>
+            </form>
+            <!-- 藏品列表 -->
+            <ul class="collectibles-list">
+                <li v-for="item in userCollectibles" :key="item.collectibleId">
+                    {{ item.collectibleId }} - {{ item.name }} - {{ item.price }}
+                </li>
+            </ul>
+            <!-- 用户列表 -->
+            <ul class="users-list">
+                <li v-for="user in users" :key="user.userId">
+                    {{ user.userId }} - {{ user.username }} - {{ user.email }}
+                    <button @click="handleEditUser(user)">编辑</button>
+                    <button @click="handleDeleteUser(user.userId)">删除</button>
+                </li>
+            </ul>
+            <!-- 编辑表单 -->
+            <div v-if="isEditing" class="edit-form">
+                <h2>编辑用户</h2>
+                <form @submit.prevent="handleUpdateUser">
+                    <input v-model="currentUser.username" placeholder="用户名" required />
+                    <input v-model="currentUser.email" type="email" placeholder="邮箱" required />
+                    <input v-model="currentUser.passwordHash" type="password" placeholder="密码" required />
+                    <button type="submit">更新</button>
+                    <button type="button" @click="handleCancelEdit">取消</button>
+                </form>
+            </div>
         </div>
     </div>
 </template>
@@ -94,4 +96,25 @@ const handleQueryUserCollectibles = async (): Promise<void> => {
 onMounted(fetchUsers);
 </script>
 
-<style scoped></style>
+<style scoped>
+.user-layout {
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+}
+
+@media (min-width: 768px) {
+    .user-layout {
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    .user-form,
+    .query-form,
+    .collectibles-list,
+    .users-list,
+    .edit-form {
+        flex: 1 1 45%;
+    }
+}
+</style>
