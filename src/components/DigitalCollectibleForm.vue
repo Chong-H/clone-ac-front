5d47b514dd92d0d250577f349691d18f64df8c96
PosedgeClk <PosedgeClock@outlook.com>
2024-12-19 15:03:38 +0800

    更改了发行数字藏品表单,添加了描述

--- src/components/DigitalCollectibleForm.vue
+++ src/components/DigitalCollectibleForm.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="wrapper">
         <form>
+            <label>owner id</label>
             <input v-model="currentCollectibleCreate.owner" type="number" placeholder="所有者ID" required />
+            <label>name</label>
             <input v-model="currentCollectibleCreate.name" placeholder="名称" required />
+            <label>description描述</label>
             <input v-model="currentCollectibleCreate.description" placeholder="描述" required />
+            <label>price价格</label>
             <input v-model="currentCollectibleCreate.price" type="number" placeholder="价格" required />
+            <label>status状态(核销/未核销)</label>
             <input v-model="currentCollectibleCreate.status" placeholder="状态" required />
+            <label>验证状态</label>
             <input v-model="currentCollectibleCreate.verificationStatus" placeholder="验证状态" required />
             <button type="button" @click="handleEdit(false)">{{ confirmButtonText }}</button>
             <button type="button" @click="handleEdit(true)">取消</button>
