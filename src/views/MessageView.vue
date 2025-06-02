7171fdbf05151922e1b26350511d54223db75800
PosedgeClk <PosedgeClock@outlook.com>
2025-06-02 22:37:03 +0800

    针对各项交易数据,验证是否遭受篡改

--- src/views/MessageView.vue
+++ src/views/MessageView.vue
@@ -2,42 +2,41 @@
     <div>
         <div class="user-layout">
             <p> User ID: {{ store.userId }}</p>
-            
+
         </div>
-        <div  >
-                <h2>账号信息</h2>
-                <form @submit.prevent="handleUpdateUser">
-                    <label>用户名:</label>
-                    <input v-model="currentUser1.username" placeholder="用户名" required />
-                    <label>邮箱:</label>
-                    <input v-model="currentUser1.email" type="email" placeholder="邮箱" required />
-                    <label>密码:</label>
-                    <input v-model="currentUser1.passwordHash" type="password" placeholder="密码" required />
-                    <button type="submit">更新</button>
-                    <button type="button" @click="handleCancelEdit">取消</button>
-                </form>
-            </div>
-            <h2>交易信息</h2>
+        <div>
+            <h2>账号信息</h2>
+            <form @submit.prevent="handleUpdateUser">
+                <label>用户名:</label>
+                <input v-model="currentUser1.username" placeholder="用户名" required />
+                <label>邮箱:</label>
+                <input v-model="currentUser1.email" type="email" placeholder="邮箱" required />
+                <label>密码:</label>
+                <input v-model="currentUser1.passwordHash" type="password" placeholder="密码" required />
+                <button type="submit">更新</button>
+                <button type="button" @click="handleCancelEdit">取消</button>
+            </form>
+        </div>
+        <h2>交易信息</h2>
         <div class="cards-wrapper">
             <TransactionRecordView v-for="trans in transHiss" :key="trans.transactionId ?? undefined" :trans="trans"
-                @read="handleRead" @unread="handleUnread" @checkTrade="handlecheck"/>
+                @read="handleRead" @unread="handleUnread" @checkTrade="handlecheck" />
         </div>
 
-        
+
         <label>下面是已读的交易记录</label>
         <hr>
         <div class="cards-wrapper">
-            <TransactionRecordView v-for="trans in transHiss1" :key="trans.transactionId ?? undefined"
-            
-        :trans="trans" @read="handleRead" @unread="handleUnread" @checkTrade="handlecheck"/>
+            <TransactionRecordView v-for="trans in transHiss1" :key="trans.transactionId ?? undefined" :trans="trans"
+                @read="handleRead" @unread="handleUnread" @checkTrade="handlecheck" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted, type Ref } from 'vue';
-import { editTransaction,cheakTrans,getAllTransactions,createUserAPI, deleteUserAPI, getAllUsersAPI, getUserByIdAPI, updateUserAPI } from '@/api';
-import { store } from '@/store'; 
+import { editTransaction, cheakTrans, getAllTransactions, createUserAPI, deleteUserAPI, getAllUsersAPI, getUserByIdAPI, updateUserAPI } from '@/api';
+import { store } from '@/store';
 import { TransactionDto } from '@/pojo/dto/TransactionDto';
 import TransactionRecordView from '@/components/TransactionRecordView.vue';
 import { User } from '@/pojo/User';
@@ -45,12 +44,12 @@ import { reactive } from 'vue';
 
 
 const init: Ref<boolean> = ref(true);
-    const isEditing = ref(false);
-    
-    
+const isEditing = ref(false);
+
+
 const transHiss: Ref<TransactionDto[]> = ref([] as TransactionDto[]);
 const transHiss1: Ref<TransactionDto[]> = ref([] as TransactionDto[]);
-    const recipientId = ref(0);
+const recipientId = ref(0);
 const handleInputChange = (value: string) => {
     recipientId.value = parseInt(value, 10);
 };
@@ -63,24 +62,24 @@ const handleCancelEdit = () => {
 };
 
 const currentUser1 = reactive({
-  username: '',
-  email: '',
-  passwordHash: ''
+    username: '',
+    email: '',
+    passwordHash: ''
 });
 
 onMounted(async () => {
-    if (store.userId !== -1) {                                   
+    if (store.userId !== -1) {
         try {
             const response = await getAllTransactions(); // 等待 Promise 解决
-          // 将 Iterable 转换为数组
-          transHiss.value = Array.from(response.data).filter(
-                trans => ( 
-                    (  trans.buyerId==store.userId&&trans.ifReadByBuyer==0 )||
-                    (  trans.sellerId==store.userId&&trans.ifReadBySeller==0)
-                 ) 
-         ).reverse();
+            // 将 Iterable 转换为数组
+            transHiss.value = Array.from(response.data).filter(
+                trans => (
+                    (trans.buyerId == store.userId && trans.ifReadByBuyer == 0) ||
+                    (trans.sellerId == store.userId && trans.ifReadBySeller == 0)
+                )
+            ).reverse();
 
-          transHiss1.value = Array.from(response.data).filter(trans => (      (  trans.buyerId==store.userId&&trans.ifReadByBuyer==1)|| (trans.sellerId==store.userId&&trans.ifReadBySeller==1  )         )).reverse();
+            transHiss1.value = Array.from(response.data).filter(trans => ((trans.buyerId == store.userId && trans.ifReadByBuyer == 1) || (trans.sellerId == store.userId && trans.ifReadBySeller == 1))).reverse();
         } catch (error) {
             console.error('获取所有交易记录失败:', error);
         }
@@ -90,12 +89,12 @@ onMounted(async () => {
 
 
 const handleUpdateUser = async (): Promise<void> => {
-    currentUser.value.username=currentUser1.username;
-    currentUser.value.email=currentUser1.email;
-    currentUser.value.passwordHash=currentUser1.passwordHash;
-  
+    currentUser.value.username = currentUser1.username;
+    currentUser.value.email = currentUser1.email;
+    currentUser.value.passwordHash = currentUser1.passwordHash;
+
     await updateUserAPI(store.userId, currentUser.value);
-    
+
     alert("修改成功");
 };
 // async function handleRead(trans: TransactionDto): Promise<void> {
@@ -110,18 +109,18 @@ const handleUpdateUser = async (): Promise<void> => {
 async function handleRead(trans: TransactionDto): Promise<void> {
     try {
         let status;
-        if(trans.buyerId==store.userId){
-        if (trans.ifReadByBuyer == 0) {
-            trans.ifReadByBuyer = 1;
+        if (trans.buyerId == store.userId) {
+            if (trans.ifReadByBuyer == 0) {
+                trans.ifReadByBuyer = 1;
+            }
+        } else if (trans.sellerId == store.userId) {
+            if (trans.ifReadBySeller == 0) {
+                trans.ifReadBySeller = 1;
+            }
         }
-    }else  if(trans.sellerId==store.userId){
-        if (trans.ifReadBySeller == 0) {
-            trans.ifReadBySeller = 1;
-        }
-    }
         // 假设editTransaction是一个异步函数，需要await
         await editTransaction(trans);
-      alert("成功已读消息");
+        alert("成功已读消息");
     } catch (error) {
         // 错误处理逻辑
         alert("An error occurred while handling read transaction");
@@ -141,21 +140,21 @@ async function handleRead(trans: TransactionDto): Promise<void> {
 
 //     await editTransaction(trans);
 
-    
+
 // }
 async function handleUnread(trans: TransactionDto): Promise<void> {
     try {
         let status;
-        if(trans.buyerId==store.userId){
-        if (trans.ifReadByBuyer == 1) {
-            trans.ifReadByBuyer = 0;
+        if (trans.buyerId == store.userId) {
+            if (trans.ifReadByBuyer == 1) {
+                trans.ifReadByBuyer = 0;
+            }
         }
-    }
-    if(trans.sellerId==store.userId){
-        if (trans.ifReadBySeller == 1) {
-            trans.ifReadBySeller = 0;
+        if (trans.sellerId == store.userId) {
+            if (trans.ifReadBySeller == 1) {
+                trans.ifReadBySeller = 0;
+            }
         }
-    }
         // 假设editTransaction是一个异步函数，需要await
         await editTransaction(trans);
         alert("成功设为未读");
@@ -171,35 +170,16 @@ async function handleUnread(trans: TransactionDto): Promise<void> {
 }
 
 async function handlecheck(trans: TransactionDto): Promise<void> {
+    var result=null;
     try {
-        let status;
-        if(trans.buyerId==store.userId){
-        if (trans.ifReadByBuyer == 1) {
-            trans.ifReadByBuyer = 0;
+        if (trans.transactionId !== null) {
+
+            result = await cheakTrans(trans.transactionId);
+
+            
+        } else {
+            alert("error");
         }
-    }
-    if(trans.sellerId==store.userId){
-        if (trans.ifReadBySeller == 1) {
-            trans.ifReadBySeller = 0;
-        }
-    }
-        // 假设editTransaction是一个异步函数，需要await
-        // await editTransaction(trans);
-        // alert("成功设为未读");
-        
-    if (trans.transactionId !== null) {
-    const result = await cheakTrans(trans.transactionId);
-    if(result.data.transactionId!=null&&result.data.transactionId>0){
-    alert(`交易信息：
-    交易ID: ${result.data.transactionId}
-    藏品ID: ${result.data.collectibleId}
-    买家ID: ${result.data.buyerId}
-    卖家ID: ${result.data.sellerId}
-    交易时间: ${result.data.transactionDate}`);
-    }else{
-        alert("Attention! fake Transation");
-    }
-}
     } catch (error) {
         // 错误处理逻辑
         alert("Attention! Block Chain not Connected");
@@ -208,8 +188,42 @@ async function handlecheck(trans: TransactionDto): Promise<void> {
         // 根据需要，可以在这里抛出错误或者返回错误信息
         throw error; // 或者不抛出，根据业务逻辑决定
     }
+    
+    if (result == null||(result!=null&&result.data.transactionId==0)) {
+            alert("Attention , Transaction not found");
 
-    //location.href = location.href;
+    }else{
+
+            if (result.data.transactionId != trans.transactionId) {
+                alert(`数据被篡改
+                交易ID: ${result.data.transactionId}
+                `);
+            }else            if (result.data.collectibleId != trans.collectibleId) {
+                alert(`数据被篡改
+                物品ID: ${result.data.collectibleId}
+                `);
+            }else            if (result.data.buyerId != trans.buyerId) {
+                alert(`数据被篡改
+                买家ID: ${result.data.buyerId}
+                 `);
+            } else            if (result.data.sellerId != trans.sellerId) {
+                alert(`数据被篡改
+                卖家ID: ${result.data.sellerId}
+                `);
+            } else            if (result.data.transactionDate != trans.transactionDate) {
+                alert(`数据被篡改
+             交易时间: ${result.data.collectibleId}
+                `);
+            }else{
+            alert(`区块链交易信息：
+                    交易ID: ${result.data.transactionId}
+                    藏品ID: ${result.data.collectibleId}
+                    买家ID: ${result.data.buyerId}
+                    卖家ID: ${result.data.sellerId}
+                    交易时间: ${result.data.transactionDate}`);
+            }
+        }
+    
 }
 
 
